7de6fb20da111dcecae635af8f942f516dd634a0
Jerameel Gonga <jerameelgonga@MacBook-Pro-de-Jerameel-2.local>
2023-11-07 22:05:21 -0300

    all changes to fix site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './core/components/footer/footer.component';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
+import NavbarComponent from './core/components/navbar/navbar.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { HomeComponent } from './features/home/home.component';
 
