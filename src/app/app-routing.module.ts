5b67cdceabbbb8c8f3d4441d3d3325ef33bddb5d
Jerameel Gonga <jerameelgonga@MacBook-Pro-de-Jerameel-2.local>
2023-11-21 18:19:53 -0300

    updating app image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './features/contact/contact.component';
+import { DevelopmentComponent } from './features/development/development.component';
 import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: 'terms-conditions', component: ContactComponent},
+  {path: 'dev', component: DevelopmentComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './core/components/footer/footer.component';
-import NavbarComponent from './core/components/navbar/navbar.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { HomeComponent } from './features/home/home.component';
+import { TermsAndConditionComponent } from './features/terms-and-condition/terms-and-condition.component';
+import { DevelopmentComponent } from './features/development/development.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './features/home/home.component';
     NavbarComponent,
     FooterComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    TermsAndConditionComponent,
+    DevelopmentComponent
   ],
   imports: [
     BrowserModule,
