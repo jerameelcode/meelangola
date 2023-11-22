fcdbe9a496a01c8575f388a1fd8001ca3490da79
domingosjadriano <domingosadriano123@gmail.com>
2023-11-23 00:17:11 +0100

    Adicionando TermsAndConditionComponent na routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ContactComponent } from './features/contact/contact.component';
 import { DevelopmentComponent } from './features/development/development.component';
 import { HomeComponent } from './features/home/home.component';
 import { ConsultoriaComponent } from './features/consultoria/consultoria.component';
+import { TermsAndConditionComponent } from './features/terms-and-condition/terms-and-condition.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'consultoria', component: ConsultoriaComponent},
   {path: 'contact', component: ContactComponent},
-  {path: 'terms-conditions', component: ContactComponent},
+  {path: 'terms-conditions', component: TermsAndConditionComponent},
   {path: 'dev', component: DevelopmentComponent}
 ];
 
