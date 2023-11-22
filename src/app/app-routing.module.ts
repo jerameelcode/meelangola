4265d6ee38b2bdfbae8de67ff2884659ff03e6fa
domingosjadriano <domingosadriano123@gmail.com>
2023-11-22 23:13:09 +0100

    Adicionando Routas do Componente: Consultoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './features/contact/contact.component';
 import { HomeComponent } from './features/home/home.component';
+import { ConsultoriaComponent } from './features/consultoria/consultoria.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'contacto', component: ContactComponent}
+  {path: 'contacto', component: ContactComponent},
+  {path: 'consultoria', component: ConsultoriaComponent}
 ];
 
 @NgModule({
